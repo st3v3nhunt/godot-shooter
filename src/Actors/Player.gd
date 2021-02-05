428d53cd0c47f5c82e3cfa1896f3897d1c4affa3
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2021-02-05 18:47:45 +0000

    Win mono (#1)
    * feat: make mono, add enemy
    
    * feat(part1): full lvl, player death, enemy death
    
    * ignore os specific mono file
    
    * Godot specific git ignores
    
    * Remove generated files

--- src/Actors/Player.gd
+++ src/Actors/Player.gd
@@ -1,28 +1,41 @@
 extends Actor
 
+export var stomp_impulse = 1500.0
+
+func _on_EnemyDetector_area_entered(area: Area2D) -> void:
+  _velocity = calculate_stomp_velocity(_velocity, stomp_impulse)
+
+# kill player
+func _on_EnemyDetector_body_entered(body: Node) -> void:
+  queue_free()
+
 func _physics_process(delta: float) -> void:
-    var is_jump_interrupted: = Input.is_action_just_released("jump") && velocity.y < 0.0
-    var direction: = get_direction()
-    velocity = calculate_move_velocity(velocity, direction, speed, is_jump_interrupted)
-    velocity = move_and_slide(velocity, Vector2.UP)
+  var is_jump_interrupted: = Input.is_action_just_released("jump") && _velocity.y < 0.0
+  var direction: = get_direction()
+  _velocity = calculate_move_velocity(_velocity, direction, speed, is_jump_interrupted)
+  _velocity = move_and_slide(_velocity, FLOOR_NORMAL)
 
 func get_direction() -> Vector2:
-    return Vector2(
-        Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
-        -1.0 if Input.get_action_strength("jump") && is_on_floor() else 0.0
-    )
+  return Vector2(
+    Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
+    -1.0 if Input.get_action_strength("jump") && is_on_floor() else 0.0
+  )
 
 func calculate_move_velocity(
-        linear_velocity: Vector2,
-        direction: Vector2,
-        speed: Vector2,
-        is_jump_interrupted: bool
-    ) -> Vector2:
-        var new_velocity: = linear_velocity
-        new_velocity.x = speed.x * direction.x
-        new_velocity.y += gravity * get_physics_process_delta_time()
-        if direction.y == -1.0:
-            new_velocity.y = speed.y * direction.y
-        if is_jump_interrupted:
-            new_velocity.y = 0.0
-        return new_velocity
+    linear_velocity: Vector2,
+    direction: Vector2,
+    speed: Vector2,
+    is_jump_interrupted: bool
+  ) -> Vector2:
+    var velocity: = linear_velocity
+    velocity.x = speed.x * direction.x
+    if direction.y != 0.0:
+      velocity.y = speed.y * direction.y
+    if is_jump_interrupted:
+      velocity.y = 0.0
+    return velocity
+
+func calculate_stomp_velocity(linear_velocity: Vector2, impulse: float) -> Vector2:
+  var velocity: = linear_velocity
+  velocity.y = -impulse
+  return velocity

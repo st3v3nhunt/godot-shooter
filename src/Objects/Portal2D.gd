49f633284372c6e29cb62748416a9f1187b13b3a
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2021-02-07 11:55:52 +0000

    feat(part2): multiple levels

---
+++ src/Objects/Portal2D.gd
@@ -0,0 +1,17 @@
+tool
+extends Area2D
+
+onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+export var next_scene: PackedScene
+
+func _on_body_entered(body: Node) -> void:
+  teleport()
+
+func _get_configuration_warning() -> String:
+  return "Next scene is required" if not next_scene else ""
+
+func teleport() -> void:
+  animation_player.play("fade_in")
+  yield(animation_player, "animation_finished")
+  get_tree().change_scene_to(next_scene)
